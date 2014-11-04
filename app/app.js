ca45aa0a45dc828bb1f91156ec2c7c2eff13843e
santiago cotto <taioloco@gmail.com>
2014-11-04 19:26:19 -0200

    changes

--- app/app.js
+++ app/app.js
@@ -1,12 +1,49 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var BASE_URL = 'http://localhost:8080/KONAConsole/api/';
+var USER_APP = 'taio/quickstarter/';
+
+var app = angular.module('MobileAngularUiExamples', [
+    "ngRoute",
+    "ngTouch",
+    "mobile-angular-ui"
+]);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {templateUrl: "templates/listEmployee.html"})
+});
+
+app.controller('MainController', function ($scope, $http) {
+
+    $scope.filter = {value: ''}; //init the filter
+
+    $scope.search = function(keyEvent) {
+        if (keyEvent.which === 13) {//enter is pressed
+            var value = $scope.filter.value;
+            if (value == '') {
+                getAllEmployees();
+            } else {
+                getEmployesByNameLike(value);
+            }
+        }
+    }
+
+    var getEmployesByNameLike = function(name) {
+        $http.get(BASE_URL + USER_APP + 'mr_Employee/find?name='+name).
+            success(function (data) {
+                console.log("getting data from server ");
+                console.log(data);
+                $scope.employees = data.data;
+            });
+    }
+
+    var getAllEmployees = function() {
+        $http.get(BASE_URL + USER_APP + 'mr_Employee').
+            success(function (data) {
+                console.log("getting data from server " + data);
+                $scope.employees = data.data;
+            });
+    }
+
+    getAllEmployees(); //call this in in the first time
+});
